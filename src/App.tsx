7e91cf7c7d1f95792d9f8ae93b79539d5836c51b
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-02-22 17:50:17 +0530

    fix: added icon in card btn

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,16 @@ function App() {
 
         <div className="card">
           <div className="left-section">
-            <img src={perfume} className='perfume-img' alt="A bottle of perfume surrounded by foliage." />
+            <img src={perfume} className='perfume-img' alt="A bottle of perfume surrounded by foliage" />
           </div>
           <div className="right-section">
-            <h1 className='category'>PERFUME</h1>
+            <h1 className='category'>Perfume</h1>
             <h1 className='title'>Gabrielle Essence Eau De Parfum</h1>
-            <p className='product-desc'>
-              A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
-            </p>
+            <div className="">
+              <p className='product-desc'>
+                A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+              </p>
+            </div>
             <div className="price-wrapper">
               <div className="">
                 <span className='sale-price'>$149.99</span>
@@ -28,18 +30,18 @@ function App() {
               </div>
 
             </div>
-            <button className='card-btn'> 
-              <IoMdCart  className='icons'/>
+            <button className='card-btn'>
+              <IoMdCart className='icons' />
               Add to Cart</button>
           </div>
         </div>
       </div>
       <footer className='footer'>
         <div className="attribution">
-        Challange by <a href='https://www.crio.do' className='link'> crio</a> Created by
-        <a href="https://github.com/Pratik-Tulkane14" className='link'>
-          Pratik
-        </a>
+          Challange by <a href='https://www.crio.do' className='link'> crio</a> Created by
+          <a href="https://github.com/Pratik-Tulkane14" className='link'>
+            Pratik
+          </a>
         </div>
       </footer>
     </>
